5c24146574ab7c010384a890ffa979e550617163
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-28 08:48:34 +0200

    chore: move entry files to src directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import("modern-normalize");
-import App from './src/components/App/App.tsx';
+import "modern-normalize";
+import App from './components/App/App.tsx'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
