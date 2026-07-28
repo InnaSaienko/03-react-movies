c336ac4966fcefc6c25dfa623525f78494b84d18
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-28 08:24:21 +0200

    fix: standardize CSS module import in Loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,8 @@
-import css from "./Loader.module.css"
+import styles from "./Loader.module.css"
 
 const Loader = () => {
     return (
-        <div className={css.text}></div>
+        <div className={styles.text}></div>
     )
 }
 
