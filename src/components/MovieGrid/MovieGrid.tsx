ccff69f7fb388a132f92a866fa17225f0c4348a8
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-29 07:34:16 +0200

    fix(MOvieGrid): improve image src link

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -6,15 +6,18 @@ export interface MovieGridProps {
   movies: Movie[];
 }
 
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+
 export const MovieGrid: React.FC<MovieGridProps> = ({ movies }) => {
   return (
     <ul className={styles.grid}>
       {movies.map((movie) => (
         <li key={movie.id} className={styles.card}>
           <img
-            src={""}
+            src={`${imageBaseUrl}${movie.poster_path}`}
             alt={movie.title}
             className={styles.image}
+            loading="lazy"
           />
           <h3 className={styles.title}>{movie.title}</h3>
         </li>
