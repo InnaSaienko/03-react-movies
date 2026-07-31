82f57cdd3e1d3a32111c919865f702a8dbc5d41d
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-31 15:57:13 +0200

    feat(App): integrate ErrorMessage and MovieModal components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,39 +3,62 @@ import styles from './App.module.css';
 import {SearchBar} from '../SearchBar/SearchBar';
 import {MovieGrid} from '../MovieGrid/MovieGrid';
 import {getMovies} from '../../services/movieService.ts';
-import toast from 'react-hot-toast';
 import type {Movie} from '../../types/movie';
 import Loader from "../Loader/Loader.tsx";
+import MovieModal from '../MovieModal/MovieModal';
+import {ErrorMessage} from "../ErrorMessage/ErrorMessage";
 
 function App() {
     const [data, setData] = useState<Movie[]>([]);
     const [query, setQuery] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
     const handleSearch = (searchQuery: string) => {
         setQuery(searchQuery);
     };
 
+    const handleMovieClick = (movie: Movie) => {
+        setSelectedMovie(movie);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedMovie(null);
+    };
+
     useEffect(() => {
         if (!query) return;
-        setLoading(true);
+        setIsError(false);
+        const controller = new AbortController();
+        setIsLoading(true);
         const fetchData = async () => {
-
             try {
-                const movies = await getMovies(query);
+                const movies = await getMovies(query, { signal: controller.signal });
+                if (movies.length === 0) {
+                    setIsError(true);
+                }
                 setData(movies);
             } catch (error) {
-                toast.error('Failed to fetch movies');
+                if (!(error instanceof Error) || error.name !== 'AbortError') {
+                    setIsError(true);
+                }
             }
-            setLoading(false);
+            setIsLoading(false);
         };
         fetchData();
+        return () => controller.abort();
     }, [query]);
 
     return (
         <div className={styles.app}>
             <SearchBar onSearch={handleSearch}/>
-            {loading ? <Loader/> : <MovieGrid movies={data}/>}
+            {isLoading && <Loader />}
+            {isError && <ErrorMessage />}
+            {!isLoading && !isError && data.length > 0 && <MovieGrid movies={data} onMovieClick={handleMovieClick} />}
+            {selectedMovie && (
+                <MovieModal movie={selectedMovie} onClose={handleCloseModal}/>
+            )}
         </div>
     )
 }
